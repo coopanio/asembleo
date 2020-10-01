d3420457186b276b6f5defbf3266eafa129a8d30
Dario Castañé <d@rio.hn>
2020-10-01 22:00:29 +0200

    Fix events helper test

--- test/helpers/events_helper_test.rb
+++ test/helpers/events_helper_test.rb
@@ -3,28 +3,33 @@
 require 'test_helper'
 
 class EventsHelperTest < ActionView::TestCase
-  attr_accessor :event, :question
+  attr_accessor :event, :question, :expected
 
   setup do
     consultation = create(:consultation, status: :opened)
     @event = create(:event, consultation: consultation)
     @question = create(:question, consultation: consultation, status: :opened)
+
+    @expected = {
+      open: "<form class=\"button_to\" method=\"post\" action=\"/questions/#{question.id}/open?event%5Bid%5D=#{event.id}\"><input type=\"hidden\" name=\"_method\" value=\"patch\" /><input class=\"btn btn-link py-0\" type=\"submit\" value=\"Obrir\" /></form>",
+      close: "<form class=\"button_to\" method=\"post\" action=\"/questions/#{question.id}/close?event%5Bid%5D=#{event.id}\"><input type=\"hidden\" name=\"_method\" value=\"patch\" /><input class=\"btn btn-link py-0\" type=\"submit\" value=\"Tancar\" /></form>"
+    }
   end
 
   subject { question_opener_closer_link(event, question) }
 
   test 'return open link when no record is found' do
-    assert_equal "<a href=\"/questions/#{question.id}/open?method=patch\">Obrir</a>", subject
+    assert_equal expected[:open], subject
   end
 
   test 'return open link' do
     create(:events_question, event: event, question: question)
-    assert_equal "<a href=\"/questions/#{question.id}/open?method=patch\">Obrir</a>", subject
+    assert_equal expected[:open], subject
   end
 
   test 'return close link' do
     create(:events_question, event: event, question: question, status: :opened)
-    assert_equal "<a href=\"/questions/#{question.id}/close?method=patch\">Tancar</a>", subject
+    assert_equal expected[:close], subject
   end
 
   test 'return no link when question is not opened' do
