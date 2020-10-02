8d822e05cd9c6a4994f2ec73231853267b48ddba
Dario Castañé <d@rio.hn>
2020-10-02 23:23:27 +0200

    Fix consultations controller test

--- test/controllers/consultations_controller_test.rb
+++ test/controllers/consultations_controller_test.rb
@@ -3,26 +3,34 @@
 require 'test_helper'
 
 class ConsultationsControllerTest < ActionDispatch::IntegrationTest
-  attr_reader :params
+  attr_reader :params, :token
 
   setup do
     @params = { consultation: { title: 'Test', description: 'Description' } }
+    @token = create(:token, :admin)
   end
 
   test 'should create consultation' do
     post consultations_url, params: params
 
-    consultation = Consultation.first
+    consultation = Consultation.last
     assert_response :redirect
+    assert session[:token].present?
+    assert_not_equal token.id, session[:token]
     assert_equal params[:consultation][:title], consultation.title
     assert_equal params[:consultation][:description], consultation.description
   end
 
-  test 'should update consultation' do
-    token = create(:token, :admin)
+  test 'should edit consultation' do
+    login
 
-    post sessions_url, params: { token: token.to_hash }
-    assert_response :redirect
+    get edit_consultation_url(token.consultation.id)
+
+    assert_response :success
+  end
+
+  test 'should update consultation' do
+    login
 
     @params = { consultation: { status: 'opened' } }
     patch consultation_url(token.consultation.id), params: params
@@ -30,5 +38,11 @@ class ConsultationsControllerTest < ActionDispatch::IntegrationTest
     consultation = Consultation.all.first
     assert_response :redirect
     assert_equal params[:consultation][:status], consultation.status
+  end
+
+  private
+
+  def login
+    post sessions_url, params: { token: token.to_hash }
   end
 end
