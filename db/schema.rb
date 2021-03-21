d4ed1ea619b65fee6e44de4005e4740eab1022a8
Dario Castañé <d@rio.hn>
2021-03-21 18:48:51 +0100

    Upgrade Rails to 6.1.3

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,43 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_194236) do
+ActiveRecord::Schema.define(version: 2021_03_21_173429) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.integer "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "consultations", force: :cascade do |t|
     t.string "title"
@@ -23,7 +51,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_194236) do
 
   create_table "events", force: :cascade do |t|
     t.integer "status", default: 0
-    t.integer "consultation_id", null: false
+    t.bigint "consultation_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
@@ -45,7 +73,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_194236) do
   create_table "options", force: :cascade do |t|
     t.string "value"
     t.string "description"
-    t.integer "question_id", null: false
+    t.bigint "question_id", null: false
     t.index ["question_id"], name: "index_options_on_question_id"
   end
 
@@ -61,8 +89,8 @@ ActiveRecord::Schema.define(version: 2020_12_04_194236) do
 
   create_table "receipts", force: :cascade do |t|
     t.text "fingerprint"
-    t.integer "token_id", null: false
-    t.integer "question_id", null: false
+    t.bigint "token_id", null: false
+    t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_receipts_on_question_id"
@@ -98,13 +126,15 @@ ActiveRecord::Schema.define(version: 2020_12_04_194236) do
   create_table "votes", force: :cascade do |t|
     t.string "value"
     t.text "fingerprint"
-    t.integer "question_id", null: false
+    t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "weight"
     t.index ["question_id"], name: "index_votes_on_question_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "events", "consultations"
   add_foreign_key "events_questions", "consultations"
   add_foreign_key "events_questions", "events"
