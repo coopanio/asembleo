6f729fa9cbee13a1acb119376f4b2e250a6fb30f
Dario Castañé <dcastane@coopanio.com>
2023-02-12 11:03:04 +0100

    fix: disable Sentry if not in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+return unless Rails.env.production?
+
 Sentry.init do |config|
+  # TODO: at some point we need to parameterize this
+  # See https://github.com/getsentry/sentry-docs/pull/1723#issuecomment-773479895
   config.dsn = 'https://68a7ebc66a2a477298509ded6f382ca4@o390133.ingest.sentry.io/5449641'
   config.enabled_environments = %w[production]
 end
