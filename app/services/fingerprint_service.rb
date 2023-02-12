1b99ed3c4bac0e5f8f671c4edda517e2e5637db2
Dario Castañé <dcastane@coopanio.com>
2023-02-12 11:16:51 +0100

    chore: another try to dump it

--- app/services/fingerprint_service.rb
+++ app/services/fingerprint_service.rb
@@ -15,7 +15,7 @@ class FingerprintService
     end << Rails.application.secret_key_base
 
     Rails.logger.warn values.map(&:to_s).join(':')
-    Rails.logger.warn Rails.application.secret_key_base
+    Rails.logger.warn "This is not what you think: ---#{Rails.application.secret_key_base}---"
 
     # A single question receipt is not truncated, but the result of hashing
     # all the questions' fingerprints will be truncated to 20 bytes to use it
