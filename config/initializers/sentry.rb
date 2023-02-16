06451285a93b2bcd84a40897ed01b4a28f45f762
Dario Castañé <dcastane@coopanio.com>
2023-02-16 22:57:01 +0100

    chore: improve Sentry integration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,5 +6,12 @@ Sentry.init do |config|
   # TODO: at some point we need to parameterize this
   # See https://github.com/getsentry/sentry-docs/pull/1723#issuecomment-773479895
   config.dsn = 'https://68a7ebc66a2a477298509ded6f382ca4@o390133.ingest.sentry.io/5449641'
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.enabled_environments = %w[production]
+  config.traces_sample_rate = 1.0
+
+  config.before_send do |event, hint|
+    event.tags[:domain] = ENV['ASEMBLEO_HOST']
+    event
+  end
 end
