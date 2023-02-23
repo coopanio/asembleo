68952e706f3af2ec8c63547b2fbe33bcb0989a2b
Dario Castañé <dcastane@coopanio.com>
2023-02-23 21:21:20 +0100

    fix: remove HTML tags from default tokens email

--- test/mailers/consultations_mailer_test.rb
+++ test/mailers/consultations_mailer_test.rb
@@ -11,8 +11,8 @@ class ConsultationsMailerTest < ActionMailer::TestCase
 
     assert_equal [to], email.to
     assert_equal "Tokens created for #{consultation.title}", email.subject
-    assert_includes email.body.to_s, "- Admin token is <strong>#{tokens.first}</strong>."
-    assert_includes email.body.to_s, "- Manager token is <strong>#{tokens.second}</strong>."
+    assert_includes email.body.to_s, "- Admin token is #{tokens.first}."
+    assert_includes email.body.to_s, "- Manager token is #{tokens.second}."
     assert_includes email.body.to_s, "Thanks,\n#{Rails.configuration.x.asembleo.title}"
   end
 end
