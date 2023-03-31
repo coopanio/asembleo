c577aa82f743cac08fa7f9fa8c2851119ae08a9f
Dario Castañé <dcastane@coopanio.com>
2023-04-01 00:13:16 +0200

    fix: only activate retry on SQLite3 exception if defined (#58)

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
-  retry_on SQLite3::BusyException, wait: :exponentially_longer
+  retry_on SQLite3::BusyException, wait: :exponentially_longer if defined?(SQLite3)
 end
