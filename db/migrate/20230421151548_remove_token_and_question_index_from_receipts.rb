8300baf65e436b9d2ab451b4783762b8e0bd591f
Dario Castañé <dcastane@coopanio.com>
2023-04-22 08:58:47 +0200

    fix: remove User#to_hash call in magic link template

--- db/migrate/20230421151548_remove_token_and_question_index_from_receipts.rb
+++ db/migrate/20230421151548_remove_token_and_question_index_from_receipts.rb
@@ -2,6 +2,6 @@
 
 class RemoveTokenAndQuestionIndexFromReceipts < ActiveRecord::Migration[7.0]
   def change
-    remove_index :receipts, name: 'index_receipts_on_token_id_and_question_id' if index_exists?(:receipts, name: 'index_receipts_on_token_id_and_question_id')
+    remove_index :receipts, name: 'index_receipts_on_token_id_and_question_id' if index_exists?(:receipts, %i[token_id question_id], name: 'index_receipts_on_token_id_and_question_id')
   end
 end
