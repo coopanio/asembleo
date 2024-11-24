755a7d9c2b3f0014b4988de11bdf8b92fcfa4b95
Dario Castañé <d@rio.hn>
2024-11-24 08:38:11 +0100

    fix(mailers): handle nil values in notifications on sessions_mailer

--- app/mailers/sessions_mailer.rb
+++ app/mailers/sessions_mailer.rb
@@ -16,14 +16,14 @@ class SessionsMailer < ApplicationMailer
 
   def template_name
     return 'user_magic_link_email' if @scope == :global
-    return 'custom_magic_link_email' if @notification.present?
+    return 'custom_magic_link_email' if @notification.present? && @notification[:body].present?
 
     'magic_link_email'
   end
 
   def subject
     return I18n.t('sessions_mailer.user_magic_link_email.subject') if @scope == :global
-    return @notification[:subject] if @notification.present?
+    return @notification[:subject] if @notification.present? && @notification[:subject].present?
 
     I18n.t('sessions_mailer.magic_link_email.subject')
   end
